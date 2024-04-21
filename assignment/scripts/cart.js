806b76c5e7c8c3ba726f4f1ea4a6e4b791927407
Kalia Vang <kaliavang511@gmail.com>
2024-04-21 17:08:00 -0500

    passing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,83 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
+//Create a global variable named basket and set it to an empty array.
+//Create a function called addItem. It should:
+//take an input parameter for a string item
+//add the new item to the global array basket.
+//return true indicating the item was added
+
+
+let basket = [];
+
+// function addItem(item){
+//         basket.push(item)
+//     return true;
+// }
+
+
+
+// Create a function called listItems. It should:
+// loop over the items in the basket array
+// console.log each individual item on a new line
+
+
+function listItems(){
+ for (x of basket)
+ console.log('list of items', x);
+ 
+}
+
+ 
+
+//  Create a function called empty. It should:
+// reset the basket to an empty array
+// do not use basket = [] to reset the array
+
+
+function empty(){
+    while (basket.length > 0){
+            basket.pop();
+    }
+    
+}
+
+
+// Create a function called isFull(). It should:
+// return false if the basket contains less than max number of items
+// return true otherwise (equal or more than maxItems)
+
+
+const maxItems = 5;
+
+function isFull(){
+    if (maxItems > basket.length){
+        return false
+    }
+    else 
+     {
+        return true;
+    }
+}
+
+
+// Update the required addItem function to:
+// Use the isFull function to prevent more than maxItems from being added to the basket.
+// If an item was added to the array, return true
+// If there was no room and the item could not be added return false
+
+function addItem(item){
+        if (!isFull()){
+            basket.push(item)
+            return true;
+        }
+
+        else{
+            return false;
+        }
+
+        
+   }
 
 
 
@@ -12,8 +89,8 @@ console.log('***** Cart Functions *****');
 
 
 
-// DO NOT MODIFY
-// Used for automated testing
+
+
 try {
     module.exports = {
         basket: typeof basket !== 'undefined' ? basket : undefined,
